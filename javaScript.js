cfc638922457c169cb6920213eb36307eb7507e6
Abdul-Raqeeb <paragongraphic0@gmail.com>
2025-05-30 13:26:07 +0500

    all work complete

--- javaScript.js
+++ javaScript.js
@@ -3,18 +3,18 @@ function sideBar() {
   let anim1 = document.getElementById("side-bar");
   let text = document.getElementById("side-text");
 
-  
+
   anim1.classList.remove("animation-open", "animation-close");
   text.classList.remove("text-fade-in", "text-fade-out");
 
   if (anim1.style.display === "none" || anim1.style.display === "") {
     anim1.style.display = "flex";
-    anim1.classList.add("animation-open"); 
-    text.classList.add("text-fade-in");    
+    anim1.classList.add("animation-open");
+    text.classList.add("text-fade-in");
     menuBar.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   } else {
-    anim1.classList.add("animation-close"); 
-    text.classList.add("text-fade-out");    
+    anim1.classList.add("animation-close");
+    text.classList.add("text-fade-out");
     menuBar.innerHTML = '<i class="fa-solid fa-bars"></i>';
 
     setTimeout(() => {
@@ -23,8 +23,8 @@ function sideBar() {
   }
 }
 
-const imagesMobile = ["Images/img6.jpg","Images/img5.webp","Images/img4.webp"]; 
-const imageDesktop = ["Images/img1.webp","Images/img2.webp","Images/img3.webp"]; 
+const imagesMobile = ["Images/img6.jpg", "Images/img5.webp", "Images/img4.webp"];
+const imageDesktop = ["Images/img1.webp", "Images/img2.webp", "Images/img3.webp"];
 let index1 = 0;
 let index2 = 0;
 
@@ -36,26 +36,87 @@ function changeImage() {
   img1.style.opacity = 0;
   img2.style.opacity = 0;
 
- 
+
   setTimeout(() => {
     index1 = (index1 + 1) % imagesMobile.length;
     img1.src = imagesMobile[index1];
-    img1.style.opacity = 1; 
-  },1000);
+    img1.style.opacity = 1;
+  }, 1000);
 
-    setTimeout(() => {
+  setTimeout(() => {
     index2 = (index2 + 1) % imageDesktop.length;
     img2.src = imageDesktop[index2];
-    img2.style.opacity = 1; 
+    img2.style.opacity = 1;
   }, 1000);
 }
 
 setInterval(changeImage, 5000);
 
- function changeQuantity(amount) {
-      const input = document.getElementById('quantity-1');
-      let value = parseInt(input.value);
-      value += amount;
-      if (value < 1) value = 1;
-      input.value = value;
+function changeQuantity(amount) {
+  const input = document.getElementById('quantity-1');
+  let value = parseInt(input.value);
+  value += amount;
+  if (value < 1) value = 1;
+  input.value = value;
+}
+
+function cardMaker() {
+  const container = document.getElementById('product-box-2');
+
+  const product = [
+    {
+      title: "Series 9 Ultra (7 in 1)",
+      price: "Rs.3,200.00",
+      oldPrice: "6,400.00",
+      image: "Images/product3-removebg-preview.png"
+    },
+    {
+      title: "HW69 Ultra 2",
+      price: "10,000.00",
+      oldPrice: "12,600.00",
+      image: "Images/product4-removebg-preview.png"
+    },
+    {
+      title: "Used Mobile",
+      price: "5,000.00",
+      oldPrice: "10,000.00",
+      image: "Images/product5-removebg-preview.png"
+    },
+    {
+      title: "Cooling Fans",
+      price: "3,000.00",
+      oldPrice: "6,000.00",
+      image: "Images/product6-removebg-preview.png"
+    },
+     {
+      title: "Apple EarBuds",
+      price: "3,000.00",
+      oldPrice: "6,000.00",
+      image: "Images/product1-removebg-preview.png"
     }
+  ];
+
+
+  product.forEach(product => {
+    const card = document.createElement('div');
+    card.className = 'card-box-2';
+    card.innerHTML = `
+         <div class="icon-box-1">
+        <a href="" class="icons-4"><i class="fa-regular fa-heart"></i></a>
+      </div>
+      <a href="" class="images-3">
+        <img src="${product.image}" alt="Error" class="images-3-1">
+      </a>
+      <a href="" class="para-4">
+        <p>${product.title}</p>
+      </a>
+      <div class="para-box-1">
+        <p class="para-5">${product.price}</p>
+        <p class="para-6"><s>${product.oldPrice}</s></p>
+      </div>
+      `;
+
+    container.appendChild(card);
+  })
+
+}
